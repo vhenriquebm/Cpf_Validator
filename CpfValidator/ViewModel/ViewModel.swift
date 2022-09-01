08514badd0f78b4d7c26e622629c6da5499bec26
Vitor <vhenriquebm@gmail.com>
2022-09-01 15:35:27 -0300

    Adicionando logica para validar o cpf

--- CpfValidator/ViewModel/ViewModel.swift
+++ CpfValidator/ViewModel/ViewModel.swift
@@ -7,27 +7,72 @@
 
 import UIKit
 
+let mask = "###.###.###-##"
+
 struct ViewModel: ViewModelProtocol {
     
     //MARK: - Public Methods
     
     public func cpfValidator (cpf: String?) -> Bool {
         
-        guard let cpf = cpf else {return false}
+    guard let cpf = cpf, let cpfConverted = Int(cpf) else {return false}
+        
+        if isValidNumberOfDigits(with: cpf) {return true}
+        
+        if isValidCpf(cpf: cpfConverted) {return true}
+        
+        if cpf.isCPF {return true}
+        
+        return false
+    }
+    
+    //MARK: - Public Methods
+    
+    private func isValidNumberOfDigits (with cpf:String) -> Bool {
+        if cpf.count == 11 {return true}
+        return false
+    }
+    
+    private func isValidCpf(cpf: Int) -> Bool {
+        
+        
 
-        if cpf.count == 11 {
-            return true
-        }
+        
+        
+        
+        //Vamos usar como exemplo, um CPF fictício "529.982.247-25".
 
+        //Primeiramente multiplica-se os 9 primeiros dígitos pela sequência
+        //5 * 10 + 2 * 9 + 9 * 8 + 9 * 7 + 8 * 6 + 2 * 5 + 2 * 4 + 4 * 3 + 7 * 2
+
+        
         return false
     }
 }
 
 
+
+
+
+
+
+//Vamos usar como exemplo, um CPF fictício "529.982.247-25".
+//
+//Validação do primeiro dígito
+//Primeiramente multiplica-se os 9 primeiros dígitos pela sequência decrescente de números de 10 à 2 e soma os resultados. Assim:
+//
+//5 * 10 + 2 * 9 + 9 * 8 + 9 * 7 + 8 * 6 + 2 * 5 + 2 * 4 + 4 * 3 + 7 * 2
+//
+//O resultado do nosso exemplo é:
+//
+//295
+
 extension String {
-    
+
     var isCPF: Bool {
+        
         let cpf = self.onlyNumbers()
+        guard cpf.count == 11 else { return false }
 
         let i1 = cpf.index(cpf.startIndex, offsetBy: 9)
         let i2 = cpf.index(cpf.startIndex, offsetBy: 10)
@@ -63,4 +108,5 @@ extension String {
                                     options: .regularExpression,
                                     range: startIndex..<endIndex)
     }
+
 }
