e6224e9ea95999e9939a531203d3230f1113120d
Vitor Henrique <vhenriquebm@gmail.com>
2022-09-05 17:48:42 -0300

    tratamento do campo de cpf para excluir pontos e hifen pronto

---
+++ CpfValidator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CpfValidator
+//
+//  Created by vitor henrique on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
